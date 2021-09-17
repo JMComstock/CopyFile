96aac4b6bbb84a75f788131fd3fe061059aa7694
Jason Comstock <71102533+JMComstock@users.noreply.github.com>
2021-09-17 15:51:44 -0400

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,64 @@
+package com.company;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.nio.file.FileSystems;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class Main {
+
+    public static void main(String[] args) {
+        try {
+            Path sourceFile = FileSystems.getDefault().getPath("Examples", "file1.txt");
+            Path copyFile = FileSystems.getDefault().getPath("Examples", "file1copy.txt");
+            Files.copy(sourceFile, copyFile);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+//        Path path = FileSystems.getDefault().getPath("WorkingDirectoryFile.txt");
+//        printFile(path);
+//
+////        Path filePath = FileSystems.getDefault().getPath("files","SubdirectoryFile.txt");
+//        Path filePath = Paths.get(".","files","SubdirectoryFile.txt");
+//        printFile(filePath);
+//
+////        filePath = Paths.get("C:\\Users\\jmcom\\Desktop\\java\\OutThere.txt");  // this will work or the code on line 21
+//        filePath = Paths.get("C:\\","Users\\", "jmcom\\", "Desktop\\", "java\\", "OutThere.txt");
+//        printFile(filePath);
+//
+//        filePath = Paths.get(".");
+//        System.out.println(filePath.toAbsolutePath());
+//
+//        Path path2 = FileSystems.getDefault().getPath(".", "files", "..", "files", "SubdirectoryFile.txt");
+//        System.out.println(path2.normalize().toAbsolutePath());
+//        printFile(path2.normalize());
+//
+//        Path path3 = FileSystems.getDefault().getPath("thisfiledoesntexist.txt");
+//        System.out.println(path3.toAbsolutePath());
+//
+//        Path path4 = Paths.get("C:\\systems\\fakePath\\", "\\abcdef", "whatever.txt");
+//        System.out.println(path4.toAbsolutePath());
+//
+//        filePath = FileSystems.getDefault().getPath("files");
+//        System.out.println("Exists = " + Files.exists(filePath)); // prints out "Exists = true"
+//
+//        System.out.println("Exists = " + Files.exists(path4));  // prints out "Exists = false"
+//
+//    }
+//
+//    private static void printFile(Path path) {
+//        try (BufferedReader fileReader = Files.newBufferedReader(path)) {
+//            String line;
+//            while((line = fileReader.readLine()) != null) {
+//                System.out.println(line);
+//            }
+//        } catch (IOException e) {
+//            System.out.println(e.getMessage());
+//            e.printStackTrace();
+//        }
+}
+
